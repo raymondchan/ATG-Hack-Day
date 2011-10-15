0481c97de0f396870aaaed265e69175f50a1064f
ibrahim okuyucu <okuyucu@gmail.com>
2011-10-15 16:38:48 -0700

    more code

--- GameDataProvider.js
+++ GameDataProvider.js
@@ -6,6 +6,18 @@
 var MemoryWall = MemoryWall || {};
 MemoryWall.GameDataProvider = function(){
 	var Me = {};//this pointer
+	Me.facebookFriendsData = {};
+	
+	//query facebook graph for friend data
+	function getFriendData(fql, callback)
+	{
+		FB.api({method: 'fql.query', query: fql},callback);	
+	}
+	
+	function prepData(category)
+	{
+		
+	}
 	
 	Me.getCategories = function () {
 		var r = [{"name":"picture","question":"Match faces"},
@@ -16,10 +28,18 @@ MemoryWall.GameDataProvider = function(){
 	};
 	
 	Me.getGameData = function(params) {
-                console.log(params);
 		if (params.category != "picture") return;
+		if (params.category == "picture") {
+			fql = 'SELECT uid, name,pic_square FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())';
+		}else if (params.category == "friends"){
+			fql = 'SELECT uid, name,pic_square FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())';
+		}else if (params.category == "education") {
+			fql = 'SELECT uid, name,pic_square FROM user WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me())';		
+		}
+		this.getFriendData(fql, function(){this.prepData(params.category)});
 		return {
 			"version":"1.0",
+			"comparator":function(o1,o2){ return (o1.matchData==o2.matchData) },
 			"data":[
 			{"url":"http://graph.facebook.com/200024/picture","matchData":"200024"},
 			{"url":"http://graph.facebook.com/200253/picture","matchData":"200024"},
